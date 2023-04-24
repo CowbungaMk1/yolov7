f3a9c2d823cea3dc33eb6987306bbf9f467d55f6
CowbungaMk1 <83180720+CowbungaMk1@users.noreply.github.com>
2023-04-24 11:11:13 -0500

    stu7ff

---
+++ SatelliteDetector.py
@@ -0,0 +1,99 @@
+import argparse
+import time
+from pathlib import Path
+
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+from numpy import random
+
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+
+
+
+def detect(weights,source,view_img=False,save_img=False,nosave = False,Device = 0):
+
+    # source, weights, view_img, = opt.source, opt.weights, opt.view_img
+    save_img = not nosave and not source.endswith('.txt')  # save inference images
+    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
+        ('rtsp://', 'rtmp://', 'http://', 'https://'))
+
+    # Directories
+
+    # (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+
+    # Initialize
+    set_logging()
+    device = select_device(device)
+    half = device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model
+    model = attempt_load(weights, map_location=device)  # load FP32 model
+    stride = int(model.stride.max())  # model stride
+    imgsz = 640
+    imgsz = check_img_size(imgsz, s=stride)  # check img_size
+
+    # if trace:
+    #     model = TracedModel(model, device, opt.img_size)
+
+    if half:
+        model.half()  # to FP16
+
+  
+    # Set Dataloader
+    vid_path, vid_writer = None, None
+    if webcam:
+        view_img = check_imshow()
+        cudnn.benchmark = True  # set True to speed up constant image size inference
+        dataset = LoadStreams(source, img_size=imgsz, stride=stride)
+    else:
+        dataset = LoadImages(source, img_size=imgsz, stride=stride)
+
+    # Get names and colors
+    names = model.module.names if hasattr(model, 'module') else model.names
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+
+    # Run inference
+    if device.type != 'cpu':
+        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+    old_img_w = old_img_h = imgsz
+    old_img_b = 1
+
+    t0 = time.time()
+    for path, img, im0s, vid_cap in dataset:
+        img = torch.from_numpy(img).to(device)
+        img = img.half() if half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+
+        # Warmup
+        if device.type != 'cpu' and (old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
+            old_img_b = img.shape[0]
+            old_img_h = img.shape[2]
+            old_img_w = img.shape[3]
+            for i in range(3):
+                model(img, augment=opt.augment)[0]
+
+        # Inference
+        with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
+            pred = model(img, augment=opt.augment)[0]
+
+        # Apply NMS
+        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+        print(pred)
+   
+
+if __name__ == '__main__':
+
+    weights = './satwts/yolov712/weights/best.pt'
+    source = './satellite/cropped_300_sbcm/'
+    with torch.no_grad():
+     
+        detect(weights=weights, source = source,view_img=True)
+
--- deploytester.py
+++ deploytester.py
@@ -38,106 +38,105 @@ def hamming2(s1, s2):
 def main():
     sat_crop = '..\Vermont_sim_data3\ cropped_600\ '
 
+
+
     # Set up for Vector Construction
 
-    for neighbor in range(4,10):
-        for threshold in range(0,1):
+  
+    cropped_image_vector = []
+    cropped_true_match = []
+    cropped_imdist = []
+    cropped_imangle = []
+    cropped_feature_class = []
+    cropped_feature_closest = []
+    for frame_name in glob.glob(sat_crop + '*.jpg', recursive=True):
 
+        label_name = frame_name[:-4] + '.txt'
+        satellite = VectorFormatter('satellite')
 
-            cropped_image_vector = []
-            cropped_true_match = []
-            cropped_imdist = []
-            cropped_imangle = []
-            cropped_feature_class = []
-            cropped_feature_closest = []
-            for frame_name in glob.glob(sat_crop + '*.jpg', recursive=True):
+        satellite.yolo_initialize(label_name, frame_name)
 
-                label_name = frame_name[:-4] + '.txt'
-                satellite = VectorFormatter('satellite')
+        if len(satellite.p_x) > neighbor :
+            satellite.k_d_tree_test(neighbor)
+            cropped_true_match.append(satellite.true_match)
+            cropped_imdist.append(satellite.im_dist)
+            cropped_imangle.append(satellite.im_angle)
+            cropped_feature_closest.append(satellite.feature_closest)
+            cropped_feature_class.append(satellite.feature_class)
 
-                satellite.yolo_initialize(label_name, frame_name)
+    binn = BinCompletion('binn')
+    binn.bin_initialize(flatten(cropped_imdist), flatten(cropped_imangle), .8, .8)
+    for i in range(len(cropped_true_match)):
+        vector_sat_all = []
+        for j in range(len(cropped_feature_class[i])):
+            vector_sat_all.append(
+                binn.vector_def(j, cropped_feature_class[i], cropped_imdist[i], cropped_imangle[i],
+                                cropped_feature_closest[i])[0])
+        cropped_image_vector.append(vector_sat_all)
 
-                if len(satellite.p_x) > neighbor :
-                    satellite.k_d_tree_test(neighbor)
-                    cropped_true_match.append(satellite.true_match)
-                    cropped_imdist.append(satellite.im_dist)
-                    cropped_imangle.append(satellite.im_angle)
-                    cropped_feature_closest.append(satellite.feature_closest)
-                    cropped_feature_class.append(satellite.feature_class)
+    test_image_dir = '..\Vermont_sim_data2\ cropped_800\ '
+    distance_ratings = []
+    hammingtotal = []
+    features_detected = []
+    times = []
+    for frame_name in glob.glob(test_image_dir + '*.jpg', recursive=True):
+        start = time.time()
+        test_label_dir = frame_name[:-4] + '.txt'
+        drone = VectorFormatter('drone')
+        drone.yolo_initialize(test_label_dir, frame_name)
+        features_detected.append(drone.true_match)
+        if len(drone.p_x) > neighbor + 1:
+            drone.k_d_tree_test(neighbor)
+            vector_drone = []
 
-            binn = BinCompletion('binn')
-            binn.bin_initialize(flatten(cropped_imdist), flatten(cropped_imangle), .8, .8)
-            for i in range(len(cropped_true_match)):
-                vector_sat_all = []
-                for j in range(len(cropped_feature_class[i])):
-                    vector_sat_all.append(
-                        binn.vector_def(j, cropped_feature_class[i], cropped_imdist[i], cropped_imangle[i],
-                                        cropped_feature_closest[i])[0])
-                cropped_image_vector.append(vector_sat_all)
+            for i in range(len(drone.feature_class)):
+                vector_drone.append(
+                    binn.vector_def(i, drone.feature_class, drone.im_dist, drone.im_angle,
+                                    drone.feature_closest)[0])
 
-            test_image_dir = '..\Vermont_sim_data2\ cropped_800\ '
-            distance_ratings = []
-            hammingtotal = []
-            features_detected = []
-            times = []
-            for frame_name in glob.glob(test_image_dir + '*.jpg', recursive=True):
-                start = time.time()
-                test_label_dir = frame_name[:-4] + '.txt'
-                drone = VectorFormatter('drone')
-                drone.yolo_initialize(test_label_dir, frame_name)
-                features_detected.append(drone.true_match)
-                if len(drone.p_x) > neighbor + 1:
-                    drone.k_d_tree_test(neighbor)
-                    vector_drone = []
+            cropped_truth = flatten(cropped_true_match)
+            vector_sat_all = flatten(cropped_image_vector)
 
-                    for i in range(len(drone.feature_class)):
-                        vector_drone.append(
-                            binn.vector_def(i, drone.feature_class, drone.im_dist, drone.im_angle,
-                                            drone.feature_closest)[0])
+            distance = []
+            for i in range(len(vector_drone)):
 
-                    cropped_truth = flatten(cropped_true_match)
-                    vector_sat_all = flatten(cropped_image_vector)
+                last_rating = threshold
 
-                    distance = []
-                    for i in range(len(vector_drone)):
+                for j in range(len(vector_sat_all)):
+                    # if vector_drone[i][0:12] == vector_sat_all[j][0:12]:
+                    # compair drone to sat, keep only features of the same classes
+                    rating = hamming2(vector_drone[i], vector_sat_all[j])
 
-                        last_rating = threshold
+                    if rating <= last_rating:
+                        distance = [rating, cropped_truth[j], drone.true_match[i]]
+                        last_rating = rating
+                hammingtotal.append(last_rating)
+                distance_ratings.append(distance)
 
-                        for j in range(len(vector_sat_all)):
-                            # if vector_drone[i][0:12] == vector_sat_all[j][0:12]:
-                            # compair drone to sat, keep only features of the same classes
-                            rating = hamming2(vector_drone[i], vector_sat_all[j])
+            times.append(time.time() - start)
+        # print(distance_ratings)
+        # print(len(binn.min_ind_dist), len(binn.min_ind_angle))
+    # print(len(distance_ratings))
+    if len(distance_ratings) >= 1:
+        tp = 0
+        fp = 0
 
-                            if rating <= last_rating:
-                                distance = [rating, cropped_truth[j], drone.true_match[i]]
-                                last_rating = rating
-                        hammingtotal.append(last_rating)
-                        distance_ratings.append(distance)
+        for i in distance_ratings:
+            if len(i) > 2:
+                if evaluate_match(i[1], i[2]) == 0:
+                    tp = tp + 1
+                else:
+                    fp = fp + 1
+        fn = len(flatten(features_detected)) - tp - fp
+        precision = tp / (tp + fp)
+        recall = tp / (tp + fn)  # also known as senitivity
+        # false_positive_rate = fp/(0+fp)
 
-                    times.append(time.time() - start)
-                # print(distance_ratings)
-                # print(len(binn.min_ind_dist), len(binn.min_ind_angle))
-            # print(len(distance_ratings))
-            if len(distance_ratings) >= 1:
-                tp = 0
-                fp = 0
+        average_time = sum(times) / len(times)
+        fps = 1 / average_time
+        if (tp != 0 or fp != 0):
 
-                for i in distance_ratings:
-                    if len(i) > 2:
-                        if evaluate_match(i[1], i[2]) == 0:
-                            tp = tp + 1
-                        else:
-                            fp = fp + 1
-                fn = len(flatten(features_detected)) - tp - fp
-                precision = tp / (tp + fp)
-                recall = tp / (tp + fn)  # also known as senitivity
-                # false_positive_rate = fp/(0+fp)
-
-                average_time = sum(times) / len(times)
-                fps = 1 / average_time
-                if (tp != 0 or fp != 0):
-
-                    print(neighbor, threshold, precision, recall, tp, fp, fps)
+            print(neighbor, threshold, precision, recall, tp, fp, fps)
 
 
 if __name__ == "__main__":
